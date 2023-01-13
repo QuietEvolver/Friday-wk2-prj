f2b88e27d045e2c64235f6cb165fc867271e451a
Vera weikel <vera.weikel@gmail.com>
2023-01-13 08:54:16 -0800

    It will add css.hidden, index links, js notes.

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+/*
+-Form gathers input from the user.
+-Branching is used to determine the result.
+-Web API object properties or methods (not including window methods for alerting, prompting, or confirming) are used to display the result to the user.
+-Event handler properties or event listeners are used to handle events.
+-When the user submits the form again (for a second or third time) previous results are hidden, cleared, or replaced.
+*/
+
