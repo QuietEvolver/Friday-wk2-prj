d92eeecb0e5bd4a18a260b9d445412bbbbbeb336
Vera weikel <vera.weikel@gmail.com>
2023-01-13 10:34:24 -0800

    It will add disply box to html, css styling and WIP: capture val for 1 question.

--- js/scripts.js
+++ js/scripts.js
@@ -6,11 +6,22 @@
 -When the user submits the form again (for a second or third time) previous results are hidden, cleared, or replaced.
 */
 
-const handleSuggestor(e){
-  e.preventDefault();
+const handleSuggestor = (event) => {
+  event.preventDefault();
 
-  const question1 = document.getElementById("question-1").innerHTML
+  const question1Val = document.getElementById("question-1").value;
+  console.log("question1Val ", question1Val):
+  const list = document.getElementById("list");
 
+  const liItem = list.innerHTML += `<li><a href="#">Item ${list.children.length + 1}</a></li>`;
+
+  console.log("li item", liItem);
+
+  if (Number.isInteger(question1) || Number.isInteger(question2)|| Number.isInteger(question3)|| Number.isInteger(question4)|| Number.isInteger(question5)){
+    document.getElementById("result").innerText = nums;
+  } else {
+    document.getElementById("result").innerText = errorMessage();
+  }
 }
 
 window.addEventListener("load", () => {
