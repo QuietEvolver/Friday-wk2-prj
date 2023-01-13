da85d09ca96cc7a1d4080aa9b98fdb7a71496f31
Vera weikel <vera.weikel@gmail.com>
2023-01-13 14:59:29 -0800

    WIP: configuring setTimeOut on js; now not displaying results.

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,21 @@
-/*
-*/
-
 function questionsEvaluation(answer){
-
-  console.log("logging first tier");
   if (answer >= 6 && answer <= 13){
-    console.log("logging IF first tier");
-    //  document.getElementById("result").innerHTML = "Python";
     return "Python";
   } else if (answer >= 14 && answer <= 22){
-    console.log("logging ElseIF 2nd tier");
-    //  document.getElementById("result").innerHTML = "Python";
     return "Javascript";
   } else if (answer >= 23 && answer <= 30){
-    console.log("logging ElseIF 2nd tier");
-    //  document.getElementById("result").innerHTML = "Python";
     return "C#";
   }
 }
 
-function errorMessage(){
-  return "ERROR MSG: Enter #s/make selections."
-}
+// function errorMessage(){
+//   return "ERROR MSG: Enter #s/make selections."
+// }
 
 const handleSuggestor = (e) => {
   e.preventDefault();
 
+  // document.querySelector("div.hidden").
   // data capture
   let questionValue1 = parseInt(document.getElementById("question-1").value);
   let questionValue2 = parseInt(document.getElementById("question-2").value);
@@ -40,28 +30,31 @@ const handleSuggestor = (e) => {
   let output = "Your total score: " + answer + 
   ".  " +  questionsEvaluation(answer) + " is your suggested programming language.";
 
-  document.getElementById("result").innerText = output;
+  // document.getElementById("result").innerText = output;
 
-  //error handling//|| Number.isInteger(question3)|| Number.isInteger(question4)|| Number.isInteger(question5)
-  // if ((Number.isInteger(questionValue1) || Number.isInteger(questionValue2))){
+  //error handling// NaN registers as integer.
+  // if ((Number.isInteger(questionValue1) || Number.isInteger(questionValue2) || Number.isInteger(questionValue3) || Number.isInteger(questionValue4) || Number.isInteger(questionValue5))){
   //   document.getElementById("result").innerText = output;
-  // } else if ((questionValue1 || questionValue2) ===  " ") {
+  // } else if (((questionValue1 || questionValue2) || (questionValue3 || questionValue4 || questionValue5)) === NaN) {
   //   document.getElementById("result").innerText = errorMessage();
   // }
+
   questionValue1 = document.getElementById("question-1").value = null;
   questionValue2 = document.getElementById("question-2").value = null;
   questionValue3 = document.getElementById("question-3").value = null;
   questionValue4 = document.getElementById("question-4").value = null;
   questionValue5 = document.getElementById("question-5").value = null;
 
-
-  
- output = setTimeout(() => {
-    output.remove(); 
+  output = setTimeout(() => {
+    // output.remove(); 
+    console.log("Set TIME OUT");
+   // document.querySelector("div#story").removeAttribute("class");
+    document.getElementById("result");
   }, 2000);
 }
 
 window.addEventListener("load", () => {
   const form = document.getElementById("form-id");
   form.addEventListener("submit", handleSuggestor);
+  // let reset = docreset.removeAttribute("class");
 });
